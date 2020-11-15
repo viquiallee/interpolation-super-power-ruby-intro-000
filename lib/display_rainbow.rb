53c82ef273f600e7a24813f81b3abda72376c0d5
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 03:12:22 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 def display_rainbow(colors)
-  puts "R: #{colors[0]}" 
-  puts "O: #{colors[1]}"
-  puts "Y: #{colors[2]}"
-  puts "G: #{colors[3]}"
-  puts "B: #{colors[4]}"
-  puts "I: #{colors[5]}"
-  puts "V: #{colors[6]}"
+  puts "R:#{colors[0]}" 
+  puts "O:#{colors[1]}"
+  puts "Y:#{colors[2]}"
+  puts "G:#{colors[3]}"
+  puts "B:#{colors[4]}"
+  puts "I:#{colors[5]}"
+  puts "V:#{colors[6]}"
 end
 
 
