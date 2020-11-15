337f6794616040cbe66b8e0a53342400fbbc62d6
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 02:20:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
-R = "red"
-O = "orange"
-Y = "Yellow"
-G = "Green"
-B = "Blue"
-I = "Indigo"
-V = "Violet"
+red = "R"
+orange = "O"
+yellow = "Y"
+green = "G"
+blue = "B"
+indigo = "I"
+violet = "V"
 def #display_rainbow(R, O, Y, G, B, I, V)
 end
