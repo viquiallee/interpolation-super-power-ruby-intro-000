84f56edd1c53ca19e3eccd465878f41ba055fa96
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 02:17:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,4 @@ B = "Blue"
 I = "Indigo"
 V = "Violet"
 def #display_rainbow(R, O, Y, G, B, I, V)
-  puts "R: #{#display_rainbow(0)}, O: #{#display_rainbow(1), Y: #{#display_rainbow(2)}, G: #{#display_rainbow(3)}, B: #{display_rainbow(4)}, I: #{#display_rainbow(5)}, V: #{#display_rainbow(6)}."
 end
