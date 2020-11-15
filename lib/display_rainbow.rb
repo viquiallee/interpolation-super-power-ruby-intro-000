834932f35670e5691b5385d793aa2cfb28aaddbe
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 02:44:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,5 @@ colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']`
 display_rainbow (colors(0), colors(1), colors(2), colors(3), colors(4), colors(5), colors(6))
   puts "R: #{colors(0)}"
   puts "O: #{colors(1)}"
+  
 end
