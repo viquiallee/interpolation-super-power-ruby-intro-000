6b9be900a42529425a1dde2b02e565c27421786a
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 02:40:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-
+colors = 
 display_rainbow ("red", "orange", "yellow", "green", "blue", "indigo", "violet")
   puts "R: #{display_rainbow(0)}, O: #{display_rainbow(1)}, Y: #{display_rainbow(2)}, G: #{display_rainbow(3)}, B: #{display_rainbow(4)}, I: #{display_rainbow(5)}, V: #{display_rainbow}."
 end
