fea7618f80ddb85a8ff9e5e0390631c138e325ca
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 02:28:28 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,4 @@
-red = "R"
-orange = "O"
-yellow = "Y"
-green = "G"
-blue = "B"
-indigo = "I"
-violet = "V"
-def #display_rainbow('red', )
-  puts 
+
+def #display_rainbow('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+  puts "R: #{#display_rainbow(0)}, O: #{#display_rainbow(1), Y: #{#display_rainbow(2)}, G: #{#display_rainbow(3)}, B: #{display_rainbow(4)}, I: #{#display_rainbow(5)}, V: #{#display_rainbow}."
 end
