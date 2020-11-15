499a395101a3a8b6d6f4a1647da4b2900cfa435e
Victoria Alee <victoriaallee@gmail.com>
2020-11-15 02:46:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,5 @@ display_rainbow (colors(0), colors(1), colors(2), colors(3), colors(4), colors(5
   puts "G: #{colors(3)}"
   puts "B: #{colors(4)}"
   puts "I: #{colors(5)}"
-  
+  puts "V: #{colors(6)}"
 end
